60f9e5c73a4f82e43d743c84abba26f1d2516813
GregTMJ <77289720+GregTMJ@users.noreply.github.com>
2022-05-08 13:35:53 +0300

    Delete E1.8 directory

--- E1.8/Start/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomeView, AboutView, ContactView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('about', AboutView.as_view(), name='about'),
-    path('Contacts', ContactView.as_view(), name='contacts'),
-]
